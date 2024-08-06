fa8dcaf2fccc7a7afc3863e5f37874d94b43ea65
Karthik Jangam <karthik30062003@gmail.com>
2024-08-07 00:22:15 +0530

    Update-4 : Updated CodeEditor component to use Redux state and dispatch for code value updates, and modified compilerSlice to include fullCode and currentCode properties, with corresponding reducer actions.

--- Client/src/redux/slices/compilerSlice.ts
+++ Client/src/redux/slices/compilerSlice.ts
@@ -1,28 +1,40 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"; 
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 export interface compilerSliceStateType {
+  fullCode: {
     html: string;
     css: string;
     javascript: string;
-    currentLanguage: "html" | "css" | "javascript";
+  };
+  currentLanguage: "html" | "css" | "javascript";
+  currentCode: string;
 }
 
-const initialState:compilerSliceStateType = {
-    html:"",
-    css: "",
-    javascript: "",
-    currentLanguage: "html",
-}; 
+const initialState: compilerSliceStateType = {
+  fullCode: {
+    html: "this is a html code",
+    css: "this is a css code",
+    javascript: "this is a js code",
+  },
+  currentLanguage: "html",
+  currentCode: "",
+};
 
 const compilerSlice = createSlice({
-    name: "compilerSlice",
-    initialState,
-    reducers: {
-        updateCurrentLanguage:(state,action:PayloadAction<compilerSliceStateType["currentLanguage"]>)=>{
-            state.currentLanguage = action.payload;   
-        }
+  name: "compilerSlice",
+  initialState,
+  reducers: {
+    updateCurrentLanguage: (
+      state,
+      action: PayloadAction<compilerSliceStateType["currentLanguage"]>
+    ) => {
+      state.currentLanguage = action.payload;
     },
+    updateCodeValue: (state, action: PayloadAction<string>) => {
+      state.fullCode[state.currentLanguage] = action.payload;
+    },
+  },
 });
 
 export default compilerSlice.reducer;
-export const { updateCurrentLanguage } = compilerSlice.actions
+export const { updateCurrentLanguage, updateCodeValue } = compilerSlice.actions;
