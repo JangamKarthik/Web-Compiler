0d6dea1076035733d123247ec83cef532661ed2c
Karthik Jangam <karthik30062003@gmail.com>
2024-08-07 01:04:30 +0530

    Update-5 : Added RenderCode component to render the HTML,CSS and JS codes

---
+++ Client/src/components/RenderCode.tsx
@@ -0,0 +1,30 @@
+import { RootState } from "@/redux/store";
+import { useSelector } from "react-redux";
+
+export default function RenderCode() {
+  const fullCode = useSelector(
+    (state: RootState) => state.compilerSlice.fullCode
+  );
+  const combinedCode = `
+    <html>
+    <style>
+    ${fullCode.css}
+    </style>
+    <body>
+    ${fullCode.html}
+    </body>
+    <script>
+    ${fullCode.javascript}
+    </script>
+    </html>
+    `;
+
+  const iframeCode = `data:text/html;charset=utf-8,${encodeURIComponent(
+    combinedCode
+  )}`;
+  return (
+    <div className="bg-white border-2 border-red-500 h-[calc(100dvh-60px)]">
+      <iframe className="w-full h-full" src={iframeCode} />
+    </div>
+  );
+}
--- Client/src/pages/Compiler.tsx
+++ Client/src/pages/Compiler.tsx
@@ -1,5 +1,6 @@
 import CodeEditor from "@/components/CodeEditor";
 import HelperHeader from "@/components/HelperHeader";
+import RenderCode from "@/components/RenderCode";
 import {
   ResizableHandle,
   ResizablePanel,
@@ -22,7 +23,7 @@ export default function Compiler() {
         className="h-[calc(100dvh-60px)] min-w-[350px]"
         defaultSize={50}
       >
-        Right side
+        <RenderCode/>
       </ResizablePanel>
     </ResizablePanelGroup>
   );
