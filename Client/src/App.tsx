24dea54eb2fe73a512fc4f3bf853aa6428d2c522
Karthik Jangam <karthik30062003@gmail.com>
2024-08-06 16:23:04 +0530

    Update-0 : Setting up the project

---
+++ Client/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  
+  return (
+    <>
+      <Button variant="default">test button</Button>
+    </>
+  )
+}
+
+export default App
