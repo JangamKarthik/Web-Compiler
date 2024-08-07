4af48c8112b4b7f864589ac38d0ae8d69663ba53
Karthik Jangam <karthik30062003@gmail.com>
2024-08-07 15:52:10 +0530

    Update-7 : Added backend and API for saving the code into MongoDB

---
+++ Server/src/lib/dbConnect.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+
+export const dbConnect = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI!,{dbName:"Web-Compiler"})
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.log("error connecting to database")
+    }
+}
